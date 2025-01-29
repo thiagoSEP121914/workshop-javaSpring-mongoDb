f80222f4edaca63a1bb94441fecbf0fbcdd4d3d5
Thiago <thiagodacostaalves35@gmail.com>
2025-01-29 12:11:01 -0300

    Atualizacao de usuario com update

--- src/main/java/com/example/demo/resources/UserResources.java
+++ src/main/java/com/example/demo/resources/UserResources.java
@@ -47,5 +47,12 @@ public class UserResources {
         return ResponseEntity.noContent().build();
     }
 
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+        User obj = service.fromDTO(objDto);
+        obj.setId(id);
+        obj = service.update(obj);
+        return ResponseEntity.noContent().build();
+    }
 
 }
