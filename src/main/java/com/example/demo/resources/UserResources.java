5493f2697f5734260cc4025e3109d477d727f260
Thiago <thiagodacostaalves35@gmail.com>
2025-01-29 13:21:57 -0300

    End point para salvar os posts de um usuario

--- src/main/java/com/example/demo/resources/UserResources.java
+++ src/main/java/com/example/demo/resources/UserResources.java
@@ -1,5 +1,6 @@
 package com.example.demo.resources;
 
+import com.example.demo.domain.Post;
 import com.example.demo.domain.User;
 import com.example.demo.dto.UserDTO;
 import com.example.demo.service.UserService;
@@ -54,5 +55,9 @@ public class UserResources {
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
     }
-
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPost(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
+    }
 }
